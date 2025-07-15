9510e1cb43c8bf7ac1be4ec3fa3fc7b4f1b93728
ybhatnagar <yash.bhatnagar@g-ushin.com>
2025-07-15 12:07:25 +0530

    code clean up

--- gui_main.py
+++ gui_main.py
@@ -66,34 +66,34 @@ canvas1.place(x=0, y=0)
 canvas1.create_text(78.0, 168.0, anchor="nw", text="Welcome to the Test Bench",
                    fill="#0066B3", font=("Inter", -32))
 
-images["tab1_image_3"] = PhotoImage(file=relative_to_assets("image_3.png", "tab1"))
-canvas1.create_image(281.0, 77.0, image=images["tab1_image_3"])
+images["minibea_logo"] = PhotoImage(file=relative_to_assets("image_3.png", "tab1"))
+canvas1.create_image(281.0, 77.0, image=images["minibea_logo"])
 
-images["tab1_image_1"] = PhotoImage(file=relative_to_assets("image_1.png", "tab1"))
-canvas1.create_image(283.0, 417.0, image=images["tab1_image_1"])
+images["version_tile"] = PhotoImage(file=relative_to_assets("image_1.png", "tab1"))
+canvas1.create_image(283.0, 417.0, image=images["version_tile"])
 
 canvas1.create_text(199.0, 271.0, anchor="nw", text="Select Version",
                    fill="#FFFFFF", font=("Inter", -24))
 
-images["tab1_image_2"] = PhotoImage(file=relative_to_assets("image_2.png", "tab1"))
-canvas1.create_image(797.0, 325.0, image=images["tab1_image_2"])
+images["photo_with_hand"] = PhotoImage(file=relative_to_assets("image_2.png", "tab1"))
+canvas1.create_image(797.0, 325.0, image=images["photo_with_hand"])
 
 # Buttons in Tab 1
-images["btn1"] = PhotoImage(file=relative_to_assets("button_1.png", "tab1"))
-btn1 = Button(tab1, image=images["btn1"], command=lambda: print("button_1 clicked"), bd=0)
-btn1.place(x=176, y=320, width=213, height=37)
+images["version1"] = PhotoImage(file=relative_to_assets("button_1.png", "tab1"))
+btn1_tab1 = Button(tab1, image=images["version1"], command=lambda: print("version1"), bd=0)
+btn1_tab1.place(x=176, y=320, width=213, height=37)
 
-images["btn2"] = PhotoImage(file=relative_to_assets("button_2.png", "tab1"))
-btn2 = Button(tab1, image=images["btn2"], command=lambda: print("button_2 clicked"), bd=0)
-btn2.place(x=176, y=380, width=213, height=37)
+images["version2"] = PhotoImage(file=relative_to_assets("button_2.png", "tab1"))
+btn2_tab2 = Button(tab1, image=images["version2"], command=lambda: print("version2"), bd=0)
+btn2_tab2.place(x=176, y=380, width=213, height=37)
 
-images["btn3"] = PhotoImage(file=relative_to_assets("button_3.png", "tab1"))
-btn3 = Button(tab1, image=images["btn3"], command=lambda: print("button_3 clicked"), bd=0)
-btn3.place(x=176, y=440, width=213, height=37)
+images["version3"] = PhotoImage(file=relative_to_assets("button_3.png", "tab1"))
+btn3_tab3 = Button(tab1, image=images["version3"], command=lambda: print("version3"), bd=0)
+btn3_tab3.place(x=176, y=440, width=213, height=37)
 
-images["btn4"] = PhotoImage(file=relative_to_assets("button_4.png", "tab1"))
-btn4 = Button(tab1, image=images["btn4"], command=lambda: print("button_4 clicked"), bd=0)
-btn4.place(x=176, y=500, width=213, height=37)
+images["version4"] = PhotoImage(file=relative_to_assets("button_4.png", "tab1"))
+btn4_tab4 = Button(tab1, image=images["version4"], command=lambda: print("version4"), bd=0)
+btn4_tab4.place(x=176, y=500, width=213, height=37)
 
 
 # ===================================================================================================================
@@ -118,78 +118,76 @@ canvas2 = tk.Canvas(
 )
 canvas2.place(x=0, y=0)
 
-# Load and place image for Tab 2
-
 # ===================================================================================================================
 # ========== Tile-1 =================================================================================================
 
-images["tab2_image_1"] = PhotoImage(file=relative_to_assets("Tile.png", "tab2")) 
-canvas2.create_image(tablet1_X, tablet1_Y, image=images["tab2_image_1"])
+images["tile_tab2"] = PhotoImage(file=relative_to_assets("Tile.png", "tab2")) 
+canvas2.create_image(tablet1_X, tablet1_Y, image=images["tile_tab2"])
 
 canvas2.create_text(73.0, 113.0, anchor="nw", text="Unlock Capacitor", fill="#FFFFFF", font=("Inter SemiBold", 20 * -1))
 canvas2.create_text(73.0, 168.0, anchor="nw", text="Unlock 100pF Capacitor", fill="#FFFFFF", font=("Inter SemiBold", 15 * -1))
 canvas2.create_text(73.0, 214.0, anchor="nw", text="Lock 100pF Capacitor", fill="#FFFFFF", font=("Inter SemiBold", 15 * -1))
 
-entry_2 = ttk.Entry(tab2_frame, style ='Background_grey.TEntry')
-entry_2.place(x=306.0, y=168.0, width=95.0, height=20.0)
+tab2_entry_1 = ttk.Entry(tab2_frame, style ='Background_grey.TEntry')
+tab2_entry_1.place(x=306.0, y=168.0, width=95.0, height=20.0)
 
 
-entry_3 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
-entry_3.place(x=306.0, y=214.0, width=95.0, height=20.0)
+tab2_entry_2 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
+tab2_entry_2.place(x=306.0, y=214.0, width=95.0, height=20.0)
 
-images["tablet_1_run_test"] = PhotoImage(file=relative_to_assets("tab_testrun_button.png", "tab2"))
-setvoltage = Button(tab2, image=images["tablet_1_run_test"], command=lambda: print("tab_testrun_button 1 ..."), bd = 0)
-setvoltage.place(x=368, y=106, width=34, height=34)
+images["tile1_run_test"] = PhotoImage(file=relative_to_assets("tab_testrun_button.png", "tab2"))
+run_test_tile1 = Button(tab2, image=images["tile1_run_test"], command=lambda: print("tile one capa run test ..."), bd = 0)
+run_test_tile1.place(x=368, y=106, width=34, height=34)
 
 # ===================================================================================================================
 # ========== Tile-2 =================================================================================================
 
-images["tab2_image_6"] = PhotoImage(file=relative_to_assets("Tile.png", "tab2")) 
-canvas2.create_image((tablet1_X + 475), (tablet1_Y + 0), image=images["tab2_image_6"])
+images["tile2_tab2"] = PhotoImage(file=relative_to_assets("Tile.png", "tab2")) 
+canvas2.create_image((tablet1_X + 475), (tablet1_Y + 0), image=images["tile2_tab2"])
 
 canvas2.create_text(560.0, 113.0, anchor="nw", text="Proximity Capacitor", fill="#FFFFFF", font=("Inter SemiBold", 20 * -1))
 canvas2.create_text(560.0, 168.0, anchor="nw", text="Unlock 100pF Capacitor", fill="#FFFFFF", font=("Inter SemiBold", 15 * -1))
 canvas2.create_text(560.0, 214.0, anchor="nw", text="Lock 100pF Capacitor", fill="#FFFFFF", font=("Inter SemiBold", 15 * -1))
 
 
-entry_4 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
-entry_4.place(x=780.0, y=168.0, width=95.0, height=20.0)
+tab2_entry3 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
+tab2_entry3.place(x=780.0, y=168.0, width=95.0, height=20.0)
 
 
-entry_5 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
-entry_5.place(x=780.0, y=214.0, width=95.0, height=20.0)
+tab2_entry4 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
+tab2_entry4.place(x=780.0, y=214.0, width=95.0, height=20.0)
 
-images["tablet_2_run_test"] = PhotoImage(file=relative_to_assets("tab_testrun_button.png", "tab2"))
-setvoltage = Button(tab2, image=images["tablet_2_run_test"], command=lambda: print("tab_testrun_button 1 ..."), bd = 0)
-setvoltage.place(x=840 , y=106, width=34, height=34)
+images["tile2_run_test"] = PhotoImage(file=relative_to_assets("tab_testrun_button.png", "tab2"))
+run_test_tile2 = Button(tab2, image=images["tile2_run_test"], command=lambda: print("tile two capa run test ..."), bd = 0)
+run_test_tile2.place(x=840 , y=106, width=34, height=34)
 
 # ===================================================================================================================
 # ========== Tile-3 =================================================================================================
 
-images["tab2_image_9"] = PhotoImage(file=relative_to_assets("Tile.png", "tab2")) 
-canvas2.create_image((tablet1_X + 0), (tablet1_Y + 235), image=images["tab2_image_9"])
+images["tile3_tab2"] = PhotoImage(file=relative_to_assets("Tile.png", "tab2")) 
+canvas2.create_image((tablet1_X + 0), (tablet1_Y + 235), image=images["tile3_tab2"])
 
 canvas2.create_text(73.0, 348.0, anchor="nw", text="Lock Capacitor", fill="#FFFFFF", font=("Inter SemiBold", 20 * -1))
 canvas2.create_text(73.0, 403.0, anchor="nw", text="Unlock 100pF Capacitor", fill="#FFFFFF", font=("Inter SemiBold", 15 * -1))
 canvas2.create_text(73.0, 449.0, anchor="nw", text="Lock 100pF Capacitor", fill="#FFFFFF", font=("Inter SemiBold", 15 * -1))
 
 
-entry_6 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
-entry_6.place(x=306.0, y=403.0, width=95.0, height=20.0)
+tab2_entry5 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
+tab2_entry5.place(x=306.0, y=403.0, width=95.0, height=20.0)
 
 
-entry_7 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
-entry_7.place(x=306.0, y=449.0, width=95.0, height=20.0)
+tab2_entry6 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
+tab2_entry6.place(x=306.0, y=449.0, width=95.0, height=20.0)
 
-images["tablet_3_run_test"] = PhotoImage(file=relative_to_assets("tab_testrun_button.png", "tab2"))
-setvoltage = Button(tab2, image=images["tablet_3_run_test"], command=lambda: print("tab_testrun_button 1 ..."), bd = 0)
-setvoltage.place(x=368, y=341, width=34, height=34)
+images["tile3_run_test"] = PhotoImage(file=relative_to_assets("tab_testrun_button.png", "tab2"))
+run_test_tile3 = Button(tab2, image=images["tile3_run_test"], command=lambda: print("tile three capa run test ..."), bd = 0)
+run_test_tile3.place(x=368, y=341, width=34, height=34)
 
 # ===================================================================================================================
 # ========== Footerbar ==============================================================================================
 
-images["tab2_image_10"] = PhotoImage(file=relative_to_assets("footer_bar.png", "tab2")) #Footer bar
-canvas2.create_image(500, 606, image=images["tab2_image_10"])
+images["tab2_footerbar"] = PhotoImage(file=relative_to_assets("footer_bar.png", "tab2")) #Footer bar
+canvas2.create_image(500, 606, image=images["tab2_footerbar"])
 
 canvas2.create_text(
     22.0,
@@ -200,12 +198,12 @@ canvas2.create_text(
     font=("Inter Bold", 16 * -1)
 )
 
-entry_8 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
-entry_8.place(x=306.0, y=580.0, width=95.0, height=20.0)
+tab2_entry7 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
+tab2_entry7.place(x=306.0, y=580.0, width=95.0, height=20.0)
 
-images["setvoltage"] = PhotoImage(file=relative_to_assets("setvoltage.png", "tab2"))
-setvoltage = Button(tab2, image=images["setvoltage"], command=lambda: print("setvoltage..."), bd = 0)
-setvoltage.place(x=413, y=576, width=25, height=26)
+images["set_voltage_button"] = PhotoImage(file=relative_to_assets("set_or_get_voltage.png", "tab2"))
+tab2_setvoltage = Button(tab2, image=images["set_voltage_button"], command=lambda: print("setvoltage..."), bd = 0)
+tab2_setvoltage.place(x=413, y=576, width=25, height=26)
 
 canvas2.create_text(
     22.0,
@@ -216,22 +214,22 @@ canvas2.create_text(
     font=("Inter SemiBold", 16 * -1)
 )
 
-entry_8 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
-entry_8.place(x=306.0, y=613.0, width=95.0, height=20.0)
+tab2_entry8 = ttk.Entry(tab2_frame, style = 'Background_grey.TEntry')
+tab2_entry8.place(x=306.0, y=613.0, width=95.0, height=20.0)
 
-images["setvoltage1"] = PhotoImage(file=relative_to_assets("setvoltage.png", "tab2"))
-setvoltage = Button(tab2, image=images["setvoltage1"], command=lambda: print("setvoltage..."), bd = 0)
-setvoltage.place(x=413, y=610, width=25, height=26)
+images["get_voltage_button"] = PhotoImage(file=relative_to_assets("set_or_get_voltage.png", "tab2"))
+tab2_getvoltage = Button(tab2, image=images["get_voltage_button"], command=lambda: print("getvoltage..."), bd = 0)
+tab2_getvoltage.place(x=413, y=610, width=25, height=26)
 
 # ===================================================================================================================
 # ========== Toolbar ================================================================================================
 
-images["tab2_image_11"] = PhotoImage(file=relative_to_assets("top_toolbar.png", "tab2")) #tool bar
-canvas2.create_image(179, 19, image=images["tab2_image_11"])
+images["tab2_toptoolbar"] = PhotoImage(file=relative_to_assets("top_toolbar.png", "tab2")) #tool bar
+canvas2.create_image(179, 19, image=images["tab2_toptoolbar"])
 
-images["toolbar_playbutton1"] = PhotoImage(file=relative_to_assets("setvoltage.png", "tab2"))
-setvoltage1 = Button(tab2, image=images["toolbar_playbutton1"], command=lambda: print("setvoltage..."), bd = 0)
-setvoltage1.place(x=182, y=5, width=26, height=26)
+images["toolbar_playbutton1"] = PhotoImage(file=relative_to_assets("set_or_get_voltage.png", "tab2"))
+toolbar_playbutton = Button(tab2, image=images["toolbar_playbutton1"], command=lambda: print("Run code ..."), bd = 0)
+toolbar_playbutton.place(x=182, y=5, width=26, height=26)
 
 canvas2.create_text(
     168.0,
@@ -244,9 +242,9 @@ canvas2.create_text(
 
 
 
-images["toolbar_pausebutton"] = PhotoImage(file=relative_to_assets("toolbar_pausebutton.png", "tab2"))
-btn5 = Button(tab2, image=images["toolbar_pausebutton"], command=lambda: print("button_5 clicked"), bd = 0)
-btn5.place(x=100, y=5, width=22.99, height=22.99)
+images["toolbar_pausebutton1"] = PhotoImage(file=relative_to_assets("toolbar_pausebutton.png", "tab2"))
+toolbar_pausebutton1 = Button(tab2, image=images["toolbar_pausebutton1"], command=lambda: print("pause code ... "), bd = 0)
+toolbar_pausebutton1.place(x=100, y=5, width=22.99, height=22.99)
 
 canvas2.create_text(
     80.0,
@@ -258,9 +256,9 @@ canvas2.create_text(
 )
 
 
-images["toolbar_exitbutton"] = PhotoImage(file=relative_to_assets("toolbar_exitbutton.png", "tab2"))
-btn5 = Button(tab2, image=images["toolbar_exitbutton"], command=lambda: print("button_6 clicked"), bd = 0)
-btn5.place(x=261, y=8, width=15, height=15)
+images["toolbar_exitbutton1"] = PhotoImage(file=relative_to_assets("toolbar_exitbutton.png", "tab2"))
+toolbar_exitbutton1 = Button(tab2, image=images["toolbar_exitbutton1"], command=lambda: print("Exit ... "), bd = 0)
+toolbar_exitbutton1.place(x=261, y=8, width=15, height=15)
 
 canvas2.create_text(
     260.0,
